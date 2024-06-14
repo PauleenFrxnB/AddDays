d1afb81070173f4f26eb0d6f8a4484d092d23ee1
paufrxn19 <164872421+paufrxn19@users.noreply.github.com>
2024-06-14 15:39:56 +0800

    Initial commit

---
+++ AddDays/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.XPath;
+
+namespace AddDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Create Methods
+
+            Console.Write("Enter a number between 1 to 7: ");
+            int dayNumber = Convert.ToInt16(Console.ReadLine());
+            string day = DetermineDay(dayNumber);
+
+            Console.Write("Input number to add day: ");
+            int dayToAdd = Convert.ToInt16(Console.ReadLine());
+        }
+        static string DetermineDay(int dayNumber)
+        {
+            string message = "";
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("The day is MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("The day is TUESDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("The day is WEDNESDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("The day is THURSDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is FRIDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("The day is SATURDAY");
+                    break;
+                case 7:
+                    Console.WriteLine("The day is SUNDAY");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input.");
+                    break;
+            }
+
+            Console.Write("Input number to add to the day: ");
+            int dayToAdd = Convert.ToInt16(Console.ReadLine());
+
+            int finalDay = (dayNumber + dayToAdd) - 7;
+
+            switch (finalDay)
+            {
+                case 1:
+                    Console.WriteLine("The day is MONDAY");
+                    break;
+                case 2:
+                    Console.WriteLine("The day is TUESDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("The day is WEDNESDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("The day is THURSDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is FRIDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("The day is SATURDAY");
+                    break;
+                case 7:
+                    Console.WriteLine("The day is SUNDAY");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input.");
+                    break;
+            }
+            return message;
+        }
+    }
+}
